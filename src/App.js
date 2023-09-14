60c10df51f27b49adcdd03d035d773681b077f05
gautam890 <gomzy890@gmail.com>
2023-09-14 14:54:20 +0530

    change implementation

--- src/App.js
+++ src/App.js
@@ -1,38 +1,43 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import './App.css';
-import ReactDOM from 'react-dom';
-import { useState } from 'react';
-
-const iframeUrl = 'https://pfw-test-ukwest-safespace.azurewebsites.net/';
+import { useEffect, useState } from 'react';
 
 function App() {
- const [isVisible, setVisible] = useState(false);
+ const [html, setHtml] = useState('');
+
+ useEffect( () => {
+  fetch('https://apps.parcelforce.com/sso/').then((resp) => {
+    return resp.text()
+  })
+  .then(function(html) {
+    setHtml(html);
+})
+.catch(function(err) {  
+    console.log('Failed to fetch page: ', err);  
+});
+ },[])
 
 
-const showiframe= (e) => {
-  setVisible(true);
-}
+const hostWidgetInIframe = () => {
+  var targetEl = document.body;
+  var iframe = document.createElement('iframe');
+  iframe.setAttribute('id', 'safe-space-iframe');
+  iframe.setAttribute('title', 'safe-space-widget-iframe');
+  targetEl.appendChild(iframe);
+  targetEl.style.overflow = "hidden";
+  iframe.setAttribute("style", "height:100%;width:100%;position:fixed;top:0;bottom:0;background-color: rgba(0, 0, 0, 0.26);");
+  iframe.contentWindow.document.open();
+  iframe.contentWindow.document.write(html);
+  iframe.contentWindow.document.close();
+  iframe.focus();
+ } 
 
   return (
     <div className="App">
       <header className="App-header">
-       <div>
-        <button onClick={showiframe}>click me!</button>
-
-        {isVisible ? ReactDOM.createPortal(
-      <div className="iframe-safe-space-popup" data-testid="iframe-safe-space" id="iframe-safe-space">
-        <iframe
-          src={iframeUrl}
-          className="safespace-iframe"
-          id="safe-space-iframe"
-          sandbox="allow-scripts allow-popups allow-same-origin"
-          title="Safe Space"
-          width="100%"
-          height="100%" />
-      </div>,
-      document.body
-    ) : ''}
-
-       </div>
+       <a  onClick={hostWidgetInIframe} href="javascript:void(0)">
+          <img src="/Assets/img/safespace.png"  alt="safe-space" />
+       </a>
       </header>
     </div>
   );
